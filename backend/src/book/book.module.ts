b64ca98894dcc577afcf5afe12df9ba08ff022e2
Le Dang Minh <minhledang.hcm@gmail.com>
2024-05-16 16:55:18 +0700

    chore: remove seed service

--- backend/src/book/book.module.ts
+++ backend/src/book/book.module.ts
@@ -1,18 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BookService } from './service/book.service';
-import { SeedService } from './service/seed.service';
 import { AnalysisService } from './service/analysis.service';
 import { AuthorService } from './service/author.service';
 import { CategoryService } from './service/category.service';
 
 @Module({
-  providers: [
-    BookService,
-    AnalysisService,
-    AuthorService,
-    CategoryService,
-    SeedService,
-  ],
+  providers: [BookService, AnalysisService, AuthorService, CategoryService],
   exports: [BookService],
 })
 export class BookModule {}
