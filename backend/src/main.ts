345479c2a1a56b4900367d95fad9ba16f7e9b51a
Le Dang Minh <minhledang.hcm@gmail.com>
2024-05-20 15:12:29 +0700

    fix: nest not hot reload on hbs file change

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,9 @@ import * as dotenv from 'dotenv';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import * as hbs from 'hbs';
 import * as cookieParser from 'cookie-parser';
+import * as hbsutil from 'hbs-utils';
+
+const utils = hbsutil(hbs);
 
 dotenv.config({
   path: `${__dirname}/../.env`,
@@ -13,9 +16,11 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.setViewEngine('hbs');
   app.use(cookieParser());
-  app.useStaticAssets(`${__dirname}/../src/admin/public`);
-  app.setBaseViewsDir(`${__dirname}/../src/admin/view`);
-  hbs.registerPartials(`${__dirname}/../src/admin/view/partial`);
+  app.useStaticAssets(`${__dirname}/admin/public`);
+  app.setBaseViewsDir(`${__dirname}/admin/view`);
+  utils.registerPartials(`${__dirname}/admin/view/partial`);
+  utils.registerWatchedPartials(`${__dirname}/admin/view/partial`)
+  utils.precompilePartials();
   await app.listen(5000);
 }
 bootstrap();
