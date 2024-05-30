0308b832e1b8b26dad0646658b59d67a760e14cc
Le Dang Minh <minhledang.hcm@gmail.com>
2024-05-30 09:24:52 +0700

    style: footer

--- frontend/src/components/app-footer.tsx
+++ frontend/src/components/app-footer.tsx
@@ -5,18 +5,50 @@ import AppContainer from './app-container';
 
 export default function AppFooter() {
 	return (
-		<footer className="h-24 bg-slate-200 relative">
+		<footer className="w-full bg-slate-200 p-8">
 			<AppContainer>
-				<div className="flex gap-2 h-full items-center">
-					<div className="h-16">
-						<Image className="!static" src="/bookworm.svg" alt="Logo" fill />
+				<div className="flex flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 text-center md:justify-between">
+					<div className="w-10">
+						<Image src="/bookworm.svg" alt="logo-ct" className="!static" fill />
 					</div>
-          <div>
-            <h3 className='text-sm font-medium uppercase'>BookWorm</h3>
-            <p className='text-xs font-normal'>Adress: 364 Cộng Hòa, Etown Tân Bình, HCM</p>
-            <p className='text-xs font-normal'>Phone: 09999999</p>
-          </div>
+					<ul className="flex flex-wrap items-center gap-y-2 gap-x-8">
+						<li>
+							<a
+								href="#"
+								className="font-normal transition-colors hover:text-blue-500 focus:text-blue-500">
+								About Us
+							</a>
+						</li>
+						<li>
+							<a
+								href="#"
+								color="blue-gray"
+								className="font-normal transition-colors hover:text-blue-500 focus:text-blue-500">
+								License
+							</a>
+						</li>
+						<li>
+							<a
+								href="#"
+								color="blue-gray"
+								className="font-normal transition-colors hover:text-blue-500 focus:text-blue-500">
+								Contribute
+							</a>
+						</li>
+						<li>
+							<a
+								href="#"
+								color="blue-gray"
+								className="font-normal transition-colors hover:text-blue-500 focus:text-blue-500">
+								Contact Us
+							</a>
+						</li>
+					</ul>
 				</div>
+				<hr className="my-8 border-blue-gray-50" />
+				<p color="blue-gray" className="text-center font-normal">
+					&copy; 2024 BookWorm
+				</p>
 			</AppContainer>
 		</footer>
 	);
