b718607a1b9937388de2fd41984344505b0b1249
Le Dang Minh <minhledang.hcm@gmail.com>
2024-06-05 17:14:56 +0700

    feat: dockerize backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -60,6 +60,6 @@ async function bootstrap() {
   utils.registerWatchedPartials(`${__dirname}/admin/view/partial`);
   utils.precompilePartials();
 
-  await app.listen(5000);
+  await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
